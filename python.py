ef2c2b56f05737dc916c2b6aef765a00a7968b66
parth151 <84718857+parth151@users.noreply.github.com>
2022-06-26 08:20:04 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,25 @@
+n = input("Enter Number:") 
+print(n)
+if n%2:
+     print("Weird")
+elif n%2 and 2<n<5:
+         print("Not Weird")
+elif n%2 and 6<n<20:
+    print("Weird")
+elif n%2 and n>20: 
+    print("Not Weird")
+
+
+
+
+
+
+
+
+   
+    
+   
+
+    
+    
+    
